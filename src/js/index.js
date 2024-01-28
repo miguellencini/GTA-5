cb605b80bc9916482d2de6e589341b190c92d873
miguellencini <134233996+miguellencini@users.noreply.github.com>
2024-01-28 18:09:55 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+    // const botao = document.querySelector(".btn-plataforma");
+    // const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+    // botao.addEventListener("click", () => {
+
+    //     const botaoEstaAberto = elementoPlataformas.classList.contains("ativo")
+    //     console.log(botaoEstaAberto);
+
+    //     if (botaoEstaAberto) {
+    //         elementoPlataformas.classList.remove("ativo");
+    //     }else {
+    //         elementoPlataformas.classList.add("ativo");
+    //     }
+
+    // });
+
+
+
+const botao = document.querySelector(".btn-plataforma");
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+});
